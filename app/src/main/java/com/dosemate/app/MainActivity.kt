8ef2e8ee9a1ec1b945be4dd969d080611777d650
kyungsoo <58407727+kyungsoo-dev@users.noreply.github.com>
2026-04-06 14:58:23 +0900

    Extract DoseMateApp into separate file

--- app/src/main/java/com/dosemate/app/MainActivity.kt
+++ app/src/main/java/com/dosemate/app/MainActivity.kt
@@ -3,16 +3,6 @@ package com.dosemate.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,28 +11,4 @@ class MainActivity : ComponentActivity() {
             DoseMateApp()
         }
     }
-}
-
-@Composable
-fun DoseMateApp() {
-    MaterialTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "Hello DoseMate!")
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DoseMatePreview() {
-    DoseMateApp()
 }
